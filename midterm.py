d5f5706e72bfa34e67f199b426be87e995c8e54f
seama107 <a8d4c3bab3dd47c89eb25e4486993de9@compute.c.sage-math-inc.internal>
2016-03-20 19:35:15 +0000

    Finished Probs 1-3

--- midterm.py
+++ midterm.py
@@ -11,29 +11,35 @@ given in eq. 1
 
 """
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def sequence(x0, r, N = 100):
     """
-    Outputs a pair of numpy arrays, one being the sequence
-    number, and the second being the value at that position
-    in the sequence given by eq. 1:
+    Outputs a numpy array of length N + 1, filled 
+    with values of the sequence given by eq.1:
     x[n+1] = r * x[n] * (1 - x[n])
     """
-    xList = np.arange(1, N+1)
-    yList = np.zeros(N)
+    yList = np.zeros(N+1)
     yList[0] = x0
-    for i in xrange(1, N):
+    for i in xrange(1, N+1):
         yList[i] = r * yList[i-1] * (1 - yList[i-1])    
-    return xList, yList
+    return yList
+
+def plotAsymtoticValues(x0, r, nMin, nMax):
+    seq = sequence(x0, r, nMax)
+    vectorized_plot = np.vectorize(plt.plot)
+    vectorized_plot(r, seq[nMin:], 'b.')
+    if(r % .01 < .001):
+        print "r = {}".format(r)
 
 def test_sequence_on_r0():
     """
     Tests sequence function on the value r = 0, where
     all values besides the initial should be zero
     """
-    sequence_output = sequence(.5, 0)[1]
-    assert np.all(sequence_output[1:] == np.zeros(99))
+    sequence_output = sequence(.5, 0)
+    assert np.all(sequence_output[1:] == np.zeros(100))
     assert (sequence_output[0] == .5)
 
 def test_sequence_on_x0():
@@ -41,16 +47,16 @@ def test_sequence_on_x0():
     Tests sequence function on the value x0 = 0, where
     all values should be zero
     """
-    sequence_output = sequence(0, 2.5)[1]
-    assert np.all(sequence_output == np.zeros(100))
+    sequence_output = sequence(0, 2.5)
+    assert np.all(sequence_output == np.zeros(101))
 
 def test_sequence_on_2():
     """
     Tests the sequence function on the values x0= .5
     and r = 2, where the output should be .5 everytime.
     """
-    sequence_output = sequence(.5, 2)[1]
-    a = np.empty(100)
+    sequence_output = sequence(.5, 2)
+    a = np.empty(101)
     a.fill(.5)
     assert np.all(sequence_output == a)
 
